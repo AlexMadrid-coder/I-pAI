08bb27e22592a4eca2e83d72c2a9bdd88b712a8d
Alejandro <alemad2108@gmail.com>
2024-05-11 12:03:54 +0200

    primary web panel now is visible -TFG --COMMIT

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -8,7 +9,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ipai" is now active!');
+	console.log('We are glad to see you :-)');
+	// Con esto conseguimos la dirección de la página web que usamos como front
+	const panelHtmlPath = vscode.Uri.file(
+		vscode.Uri.joinPath(context.extensionUri, 'src', 'index.html').fsPath
+	);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -18,8 +23,26 @@ export function activate(context: vscode.ExtensionContext) {
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from I-pAI!');
 	});
+	// En esta vamos a crear el panel cuando le demos al comando
+	let disposable_view = vscode.commands.registerCommand('ipai.showView', () => {
+		// Creamos y mostramos el panel
+		const panel = vscode.window.createWebviewPanel(
+			'ipai-panel',
+			'I-pAI',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true,
+			}
+		);
+		// Asignamos el contenido html al panel view, como es una uri lo sacamos primero cone l fs.readFile
+		panel.webview.html = fs.readFileSync(panelHtmlPath.fsPath, 'utf-8');
+	});
 
-	context.subscriptions.push(disposable);
+	
+
+	// Add commands to the application
+	context.subscriptions.push(disposable); // <-- Hello world command
+	context.subscriptions.push(disposable_view); // <-- Open webview command
 }
 
 // This method is called when your extension is deactivated
