b299a01462955ad3e7b1faa77a73ecf225bde5a4
Alejandro <alemad2108@gmail.com>
2024-05-13 23:20:14 +0200

    --CONTROL-COMMIT

--- src/extension.ts
+++ src/extension.ts
@@ -4,13 +4,47 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
 
-let myPanel : vscode.WebviewPanel | undefined;
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-		
+	
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(
+			'ipai-viewContainer',
+			new MiProvider(),
+			{
+				webviewOptions: {retainContextWhenHidden: true}
+			}
+		)
+	);
+
 }
 
+// tenemos que definir la clase del provider de WebviewViewProvider
+class MiProvider implements vscode.WebviewViewProvider {
+	public resolveWebviewView(webviewView: vscode.WebviewView): void {
+		const panel = webviewView.webview;
+
+		// cargamos el contenido html del fichero
+		panel.html = 
+		`
+		<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>I-pAI</title>
+</head>
+<body>
+    <div id="app">
+        hola
+    </div>
+</body>
+</html>
+		`;
+		console.log(panel.html);
+	}
+}
 // This method is called when your extension is deactivated
+
 export function deactivate() {}
