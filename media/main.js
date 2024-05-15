4aaabc25cc7670e406dd6f58db0bd3d9347b88fc
alejandro <alemad2108@gmail.com>
2024-05-15 11:38:27 +0200

    continuación con parte visual de la extensión pero no se muestran los iconos de la carpeta de imagenes  -TFG-WORK --CONTROL-COMMIT

--- media/main.js
+++ media/main.js
@@ -1,3 +1,15 @@
-document.getElementById('ipaiButton').addEventListener('click', () => {
-    document.getElementById('ipaiButton').setAttribute('disabled', 'disabled');
+document.addEventListener('DOMContentLoaded', function () {
+    const  buttons = document.querySelectorAll('.action-button');
+    buttons.forEach(button => {
+        button.addEventListener('click', () => {
+            console.log(`Button ${button.textContent} clicked`);
+        });
+    });
+});
+
+const input = document.getElementById('userInput');
+input.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        console.log(`Input submitted: ${input.value}`);
+    }
 });
