df81ffc264c88f05c378ea9ba23593ca36c091d9
alejandro <alemad2108@gmail.com>
2024-05-20 11:03:13 +0200

    haciendo que se muestre la pregunta cuando le doy a enter o al botón de enviar -WORK-TFG --CONTROL-COMMIT

--- media/main.js
+++ media/main.js
@@ -1,6 +1,23 @@
 /** Todos los listeners */
-// Listener para cambiar automáticamente el tamaño del textaure 'ipai-sendText'
+/**
+ * Listener para cambiar el alto del campo ipai-sendText cuando metemos un salto de línea
+ */
 document.getElementById('ipai-sendText').addEventListener('input', function () {
     this.style.height = 'auto';                     // Resetea la altura para recalcularle
     this.style.height = this.scrollHeight + 'px';   // Ajustamos la altura del textarea para el contenido 
 });
+/**
+ * 
+ */
+
+// 
+
+// Definición de funciones
+/**
+ *  Esta función crea la instancia pregunta del dataset y la muestra por pantalla
+ */
+const askIpAI = () => {
+    textarea = document.getElementById('ipai-sendText').value;
+    divQuery = document.getElementById('block-query');
+    
+};
