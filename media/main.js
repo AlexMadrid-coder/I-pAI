188e7e6aad58a740b5e9f05b5d3efdbe26f1bd37
Alejandro <alemad2108@gmail.com>
2024-05-27 10:16:53 +0200

    -WORK-TFG --CONTROL-COMMIT

--- media/main.js
+++ media/main.js
@@ -7,17 +7,50 @@ document.getElementById('ipai-sendText').addEventListener('input', function () {
     this.style.height = this.scrollHeight + 'px';   // Ajustamos la altura del textarea para el contenido 
 });
 /**
- * 
+ * Listene -->'enter' en el textarea
  */
-
+document.getElementById('ipai-sendText').addEventListener('keydown', function (event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+        event.preventDefault();
+        ipai_ASK();
+    }
+});
+/**
+ * Listener --> Botón de enviar del areatext
+ */
+document.getElementById('ipai-sendButton').addEventListener('click', function() {
+    ipai_ASK();
+});
 // 
 
 // Definición de funciones
 /**
- *  Esta función crea la instancia pregunta del dataset y la muestra por pantalla
+ * Esta función es la encargada de aplicar los cambios necesarios para los listeners
+ *  del 'areatext' y del 'sendbutton'
  */
-const askIpAI = () => {
-    textarea = document.getElementById('ipai-sendText').value;
-    divQuery = document.getElementById('block-query');
-    
+function ipai_ASK() {
+    // Sacamos la información del div que aloja todos los query
+    const divsQuery = document.getElementById('block-query');
+    // Ahora cogemos todos sus hijos ( preguntas y respuestas del LLE )
+    const childs = divsQuery.childs();
+    // Ahora si tenemos el id 'ipai-div-initial' --> Eliminamos y mandamos pregunta
+    const initialDiv = document.getElementById('ipai-div-initial');
+    if (initialDiv) {
+        divsQuery.removeChild(initialDiv);
+    }
+
+    // Creamos un nuevo div para la pregunta
+    const userInput = document.getElementById('ipai-sendText').ariaValueMax.trim();
+    if (userInput) {
+        const newDiv = document.createElement('div');
+        newDiv.id = 'user-query';
+        newDiv.className = 'user-query';
+        newDiv.textContent = userInput;
+        divsQuery.appendChild(newDiv);
+
+        // Limpiamos el textArea
+        document.getElementById('ipai-sendText').value = '';
+        // Reset de altura
+        document.getElementById('ipai-sendText').style.height = 'auto';
+    }
 };
