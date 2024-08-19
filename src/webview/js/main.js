699f7c46ac952c17b04e566959e86ca967207f9f
Alejandro <alemad2108@gmail.com>
2024-08-19 18:15:32 +0200

    end of the day -TFG --CONTROL-COMMIT

--- src/webview/js/main.js
+++ src/webview/js/main.js
@@ -1,17 +1,4 @@
 /**
- * Esta función en tiempo real se encarga de evitar el cambio entre páginas o cargar contenido sobre la página principal teniendo todo
- *  el contenido escrito sobre la principal y solo mostrar la seleccionada en la barra de navegación principal
+ * voy a anadir manualmente los listerners para gestionar el movimiento entre secciones
+ * 
  */
-document.querySelectorAll('.nav-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        // Primero ocultamos todas las secciones
-        document.querySelectorAll('.content-section').forEach(section => {
-            section.classList.remove('active');
-        });
-        // Ahora mostramos la sección correspondiente
-        const target = document.querySelector(this.getAttribute('data-target'));
-        target.classList.add('active');
-    });
-});
